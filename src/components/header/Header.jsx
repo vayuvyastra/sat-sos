a98e85215464c80941bfb9b5b9dc036cd4ec80a1
Pavan-B-N <pavangowda56005@gmail.com>
2022-10-11 20:48:45 +0530

    header edited

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,14 @@
 import "./Header.css";
-
+import revalogo from "../../images/reva_logo.svg"
+import VayuVyastra_circle from "../../images/VayuVyastra_circle.png"
 function Header() {
-  return <div className="app-header"></div>;
+  return <div className="app-header">
+    <div className="left-side-app-header" >
+      <img src={revalogo} alt="" className="revalogo" />
+        <img src={VayuVyastra_circle} alt="" className="revalogo" />
+        <h2 className="title" >Vayuyastra</h2>
+    </div>
+  </div>;
 }
 
 export default Header;
